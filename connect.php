814801a2b6bd7347ee5cf22f3979f5c0cd91675c
Jason Fong <jasonfng7@gmail.com>
2022-08-02 23:16:56 -0400

    gojoanime_backend
     Initial commit
     Changes to be committed:
    	new file:   connect.php
    	new file:   gojoanime_api.js
    	new file:   register.php
    	new file:   table_creation.sql

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+	//database information 
+    $server 	= "localhost"; 
+	$user 		= "root";
+	$pass 		= "";
+    $database 	= "baseball";
+         
+	//make the database connection 
+	$mysqli = new mysqli($server, $user, $pass, $database);
+
+	if ($mysqli->connect_error) 
+		die("Connect Error " . $mysqli->connect_errno . ": " . $mysqli->connect_error);
+
+
+?>
---
+++ table_creation.sql
@@ -0,0 +1,19 @@
+create table gojoanimelogin (
+    iduserlogin NUMBER(38,0) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    loginpasswd VARCHAR2(45 BYTE) NOT NULL,
+    PRIMARY KEY (iduserlogin)
+);
+
+create table customerinfo (
+   idcustomer number(38,0) NOT NULL,
+   customername varchar(45) NOT NULL,
+   customerusername int NOT NULL,
+   phonenumber varchar(35) NOT NULL,
+   customerpasswd int NOT NULL,
+   email varchar(35),
+   PRIMARY KEY (idcustomer),
+   constraint fk_cinfo_gjl FOREIGN KEY (customerusername) REFERENCES gojoanimelogin (iduserlogin),
+   constraint fk_cinfo_gj2 FOREIGN KEY (customerpasswd) REFERENCES gojoanimelogin (iduserlogin)
+);
+
